2ffee1f98065fce7a38b0525eaf4b5b8fa1f7c5c
pfefferle <pfefferle@gmail.com>
2010-08-17 05:40:07 -0700

    * switched from "substr" to "preg_match" for a better property detection * added a public "parse" function to support HTML parsing without the need of a public url

--- OpenGraph.php
+++ OpenGraph.php
@@ -22,22 +22,22 @@ class OpenGraph implements Iterator
    * a map so that the schema can be obtained
    *
    */
-	public static $TYPES = array(
-		'activity' => array('activity', 'sport'),
-		'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
-		'group' => array('cause', 'sports_league', 'sports_team'),
-		'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
-		'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
-		'place' => array('city', 'country', 'landmark', 'state_province'),
-		'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
-		'website' => array('blog', 'website'),
-	);
+  public static $TYPES = array(
+    'activity' => array('activity', 'sport'),
+    'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
+    'group' => array('cause', 'sports_league', 'sports_team'),
+    'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
+    'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
+    'place' => array('city', 'country', 'landmark', 'state_province'),
+    'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
+    'website' => array('blog', 'website'),
+  );
 
   /**
    * Holds all the Open Graph values we've parsed from a page
    *
    */
-	private $_values = array();
+  private $_values = array();
 
   /**
    * Fetches a URI and parses it for Open Graph data, returns
@@ -46,9 +46,9 @@ class OpenGraph implements Iterator
    * @param $URI    URI to page to parse for Open Graph data
    * @return OpenGraph
    */
-	static public function fetch($URI) {
-		return self::_parse(file_get_contents($URI));
-	}
+  static public function fetch($URI) {
+    return self::_parse(file_get_contents($URI));
+  }
 
   /**
    * Parses HTML and extracts Open Graph data, this assumes
@@ -57,33 +57,44 @@ class OpenGraph implements Iterator
    * @param $HTML    HTML to parse
    * @return OpenGraph
    */
-	static private function _parse($HTML) {
-		$old_libxml_error = libxml_use_internal_errors(true);
+  static public function parse($HTML) {
+    return self::_parse($HTML);
+  }
 
-		$doc = new DOMDocument();
-		$doc->loadHTML($HTML);
-		
-		libxml_use_internal_errors($old_libxml_error);
+  /**
+   * Parses HTML and extracts Open Graph data, this assumes
+   * the document is at least well formed.
+   *
+   * @param $HTML    HTML to parse
+   * @return OpenGraph
+   */
+  static private function _parse($HTML) {
+    $old_libxml_error = libxml_use_internal_errors(true);
 
-		$tags = $doc->getElementsByTagName('meta');
-		if (!$tags || $tags->length === 0) {
-			return false;
-		}
+    $doc = new DOMDocument();
+    $doc->loadHTML($HTML);
 
-		$page = new self();
+    libxml_use_internal_errors($old_libxml_error);
 
-		foreach ($tags AS $tag) {
-			if ($tag->hasAttribute('property') &&
-			    strpos($tag->getAttribute('property'), 'og:') === 0) {
-				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
-				$page->_values[$key] = $tag->getAttribute('content');
-			}
-		}
+    $tags = $doc->getElementsByTagName('meta');
+    if (!$tags || $tags->length === 0) {
+      return false;
+    }
 
-		if (empty($page->_values)) { return false; }
-		
-		return $page;
-	}
+    $page = new self();
+
+    foreach ($tags AS $tag) {
+      if ($tag->hasAttribute('property') &&
+          preg_match('/(\s|^)og:(\S*)(\s|$)/i', $tag->getAttribute('property'), $matches)) {
+        $key = strtr($matches[2], "-", "_");
+        $page->_values[$key] = $tag->getAttribute('content');
+      }
+    }
+
+    if (empty($page->_values)) { return false; }
+
+    return $page;
+  }
 
   /**
    * Helper method to access attributes directly
@@ -92,63 +103,63 @@ class OpenGraph implements Iterator
    *
    * @param $key    Key to fetch from the lookup
    */
-	public function __get($key) {
-		if (array_key_exists($key, $this->_values)) {
-			return $this->_values[$key];
-		}
-		
-		if ($key === 'schema') {
-			foreach (self::$TYPES AS $schema => $types) {
-				if (array_search($this->_values['type'], $types)) {
-					return $schema;
-				}
-			}
-		}
-	}
+  public function __get($key) {
+    if (array_key_exists($key, $this->_values)) {
+      return $this->_values[$key];
+    }
+
+    if ($key === 'schema') {
+      foreach (self::$TYPES AS $schema => $types) {
+        if (array_search($this->_values['type'], $types)) {
+          return $schema;
+        }
+      }
+    }
+  }
 
   /**
    * Return all the keys found on the page
    *
    * @return array
    */
-	public function keys() {
-		return array_keys($this->_values);
-	}
+  public function keys() {
+    return array_keys($this->_values);
+  }
 
   /**
    * Helper method to check an attribute exists
    *
    * @param $key
    */
-	public function __isset($key) {
-		return array_key_exists($key, $this->_values);
-	}
+  public function __isset($key) {
+    return array_key_exists($key, $this->_values);
+  }
 
   /**
    * Will return true if the page has location data embedded
    *
    * @return boolean Check if the page has location data
    */
-	public function hasLocation() {
-		if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
-			return true;
-		}
-		
-		$address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
-		$valid_address = true;
-		foreach ($address_keys AS $key) {
-			$valid_address = ($valid_address && array_key_exists($key, $this->_values));
-		}
-		return $valid_address;
-	}
+  public function hasLocation() {
+    if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
+      return true;
+    }
+
+    $address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
+    $valid_address = true;
+    foreach ($address_keys AS $key) {
+      $valid_address = ($valid_address && array_key_exists($key, $this->_values));
+    }
+    return $valid_address;
+  }
 
   /**
    * Iterator code
    */
-	private $_position = 0;
-	public function rewind() { reset($this->_values); $this->_position = 0; }
-	public function current() { return current($this->_values); }
-	public function key() { return key($this->_values); }
-	public function next() { next($this->_values); ++$this->_position; }
-	public function valid() { return $this->_position < sizeof($this->_values); }
+  private $_position = 0;
+  public function rewind() { reset($this->_values); $this->_position = 0; }
+  public function current() { return current($this->_values); }
+  public function key() { return key($this->_values); }
+  public function next() { next($this->_values); ++$this->_position; }
+  public function valid() { return $this->_position < sizeof($this->_values); }
 }
